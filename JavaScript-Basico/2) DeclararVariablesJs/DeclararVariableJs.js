cf74010f85259c65f4fc04f3468ebd4b2fa5d0ce
Agustin Sanchez <aghuz2018@gmail.com>
2022-07-09 13:34:22 -0300

    2022

--- JavaScript-Basico/2) DeclararVariablesJs/DeclararVariableJs.js
+++ JavaScript-Basico/2) DeclararVariablesJs/DeclararVariableJs.js
@@ -3,4 +3,4 @@ En JavaScript terminamos las declaraciones con punto y coma.
 Los nombres de variables pueden estar formados por números, letras y $o _, pero no pueden contener espacios ni comenzar con un número. 
 */
 
-let ourName;
+let ourNamee;
