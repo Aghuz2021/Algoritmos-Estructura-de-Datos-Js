798aacb5b0da08240ba4a33a4912ca089f23a1cc
Agustin Sanchez <aghuz2018@gmail.com>
2022-07-09 13:36:32 -0300

    2022

---
+++ Js-basico/2) DeclararVariablesJs/DeclararVariableJs.js
@@ -0,0 +1,6 @@
+/* Use la palabra clave var  para crear una variable llamada myName.. 
+En JavaScript terminamos las declaraciones con punto y coma. 
+Los nombres de variables pueden estar formados por números, letras y $o _, pero no pueden contener espacios ni comenzar con un número. 
+*/
+
+let ourNamee;
---
+++ Js-basico/5) InicializarVariables-con-Operador-Asignación/InicializarVariableOperadorAsiganacion.js
@@ -0,0 +1,2 @@
+//Defina una variable a y  inicialícela con un valor de 9.
+let a = 9;
